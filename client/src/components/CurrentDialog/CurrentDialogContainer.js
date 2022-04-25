4ea3705b6619f56a2cd21b1dc7d316fcb1116671
Dmitry Korolev <crossexplorer@mail.ru>
2022-04-25 19:23:21 +0300

    Clearing CurrenDialogContainer from comments

--- client/src/components/CurrentDialog/CurrentDialogContainer.js
+++ client/src/components/CurrentDialog/CurrentDialogContainer.js
@@ -4,12 +4,10 @@ import { Link, NavLink, Routes, useParams, useNavigate } from "react-router-dom"
 import {Message, ObservableMessage} from "../Message/Message"
 import { loadDialogThunkCreator, setCurrentDialogThunkCreator, sendMessageCreator } from "../../redux/dialogsReducer"
 import css from "./CurrentDialog.module.css"
-import { WithObservation } from '../../HOCs/withObservation'
 import DialogMessages from "./DialogMessages/DialogMessages"
 import { Route } from "react-router-dom"
 
 import FindedUsers from '../FindedUsers/FindedUsers'
-import Searcher from "../Searcher/Searcher"
 import { useResizeObserver } from "../../customHooks/useResizeObserver"
 
 
@@ -24,8 +22,6 @@ const CurrentDialogContainer = (props) => {
     useEffect(() => {props.loadDialog(params.dialogId)}, [dialog])
     if (Boolean(props.dialogsMessages[params.dialogId])) return <CurrentDialogPresentation
         me = {props.me}
-        // dialog = {props.dialogsMessages[params.dialogId]} 
-        // lastReadOrder = {(props.lastReadMessages.hasOwnProperty(params.dialogId)) ? props.lastReadMessages[params.dialogId].order: null}
         dialogId = {params.dialogId}
         interlocutors = {props.interlocutors[params.dialogId]} 
         sendMessage = {(message) => {props.sendMessage(message, params.dialogId)}}
@@ -40,7 +36,6 @@ const mapStateToProps = (state) => {
         interlocutors: state.dialogs.interlocutors,
         dialogsMessages: state.dialogs.dialogsMessages,
         me: state.auth.nickname,
-        // lastReadMessages: state.dialogs.lastReadMessages
     }
 }
 
@@ -58,23 +53,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(CurrentDialogContain
 
 const CurrentDialogPresentation = (props) => {
     const navigate = useNavigate()
-    // НУЖНОЕ
-    // const toUnreadRef = useRef()
-    // const endRef = useRef()
-    // const downButtonRef = useRef()
-    // const dialogWindowRef = useRef()
     const menuButtonRef = useRef()
-    // СКОРЕЕ ВСЕГО НЕВАЖНОЕ
-    // useEffect(() => {if (toUnreadRef.current) toUnreadRef.current.scrollIntoView({block: "center", behavior: "smooth"})}, [props.dialog])
-    // useEffect(() => {
-    //     console.log('---')
-    //     console.log()
-    //     console.log(dialogWindowRef.current.clientHeight)
-    //     console.log(dialogWindowRef.current)
-        
-    //     // if (dialogWindowRef.current.scrollHeight - dialogWindowRef.current.scrollTop - (dialogWindowRef.current.scrollHeight - (dialogWindowRef.current.scrollTop + dialogWindowRef.current.clientHeight))  === dialogWindowRef.current.clientHeight)
-    //     //     endRef.current.scrollIntoView({block: "center", behavior: "smooth"})
-    // })
 
 
     const [memorizedLastReadOrder, setMemorizedLastReadOrder] = useState(props.lastReadOrder)
@@ -84,37 +63,7 @@ const CurrentDialogPresentation = (props) => {
     const [isMenuRequested, toggleMenuStatus] = useState(false)
 
     const [prevMessagesWindowHeigth, setPrevMessagesWindowHeigth] = useState(0)
-    // ВАЖНОЕ
-    // useEffect(() => {    
 
-        
-    //     const observer = new IntersectionObserver(([entry], observer) => {
-    //         if (entry.isIntersecting){
-    //             entry.target.scrollIntoView({block: "center", behavior: "smooth"})
-    //             observer.unobserve(entry.target)
-    //         }
-    //         else if (props.dialog.length !== props.lastReadOrder && isScrolled /*&& props.dialog[-1].sender !== props.me*/) downButtonRef.current.className = css.downButtonActive
-    //     })
-    //     observer.observe(endRef.current.previousElementSibling.previousElementSibling)
-        
-        
-    //     if (!isScrolled) {
-    //         toUnreadRef.current.className = (props.dialog.length !== memorizedLastReadOrder) ? css.withUnread : css.noUnread
-    //         toUnreadRef.current.scrollIntoView({block: "center", behavior: "smooth"})
-    //         setInterval( () => {setIsScrolled(true)}, 1000)
-    //     }
-        
-
-    // },[props.dialog])
-
-
-    // НЕПОНЯТНО, ВАЖНОЕ ИЛИ НЕТ...
-    // useEffect(() => {
-    //     setMemorizedLastReadOrder( prev => (typeof(prev) == undefined) ? prev : props.lastReadOrder)
-    //     console.log(props.lastReadOrder)
-    //     // debugger
-    //     // if (memorizedLastReadOrder == null) setMemorizedLastReadOrder(props.lastReadOrder)
-    // }, props.lastReadOrder)
     
     const [input, setInput] = useState({
         message: '',
@@ -142,11 +91,7 @@ const CurrentDialogPresentation = (props) => {
         return messages
     }
 
-    // ВАЖНОЕ
-    // const downButtonHandler = () => {
-    //     toUnreadRef.current.scrollIntoView({block: "center", behavior: "smooth"})
-    //     downButtonRef.current.className = css.downButtonPassive
-    // }
+
 
 
 
@@ -160,7 +105,6 @@ const CurrentDialogPresentation = (props) => {
     const [isViewResized, startObserving] = useResizeObserver(() => {}, document.documentElement)
     useEffect(() => {startObserving()}, [startObserving])
 
-    // if (Boolean(props.dialog)){
         return  <div ref = {wrapperRef} className = {isViewResized && document.documentElement.clientWidth < 700 ? css.smartphoneInputFocusWrapper : css.wrapper}>
                     <div className = {css.dialogHeaderWrapper}>
                     <div id = {css.backButton} onClick = {() => {navigate(-1)}}>&crarr;</div>
@@ -181,20 +125,9 @@ const CurrentDialogPresentation = (props) => {
                         <Route path = "/" element = {<DialogMessages dialogId = {props.dialogId}/>}/>
                         <Route path = "/addnewuser/" element = {<Searcher dialogId = {props.dialogId} alreadyAdded = {[...props.interlocutors, props.me] } />}/>
                     </Routes>
-                    {/* НУЖНОЕ */}
-                    {/* <div ref = {dialogWindowRef} className = {css.messages}>
-                        {getMessages(props.dialog, 0, memorizedLastReadOrder)}
-                        <div ref = {toUnreadRef} className = {css.noUnread}>Новые сообщения</div>
-                        {getMessages(props.dialog, memorizedLastReadOrder, props.dialog.length)}
-                        <div ref = {endRef}></div>
-                        <div ref = {downButtonRef} className = {css.downButtonPassive} onClick = {downButtonHandler} >[Новое сообщение]</div>    
-                        </div>
-                    <div><input placeholder = 'Введите сообщение' type = "text" class = "field" name = "email" onChange = {changeHandler}></input></div>
-                    <button class = "button" onClick = {() => {props.sendMessage(input.message);toUnreadRef.current.scrollIntoView({block: "center", behavior: "smooth"})}} >Отправить.</button> */}
                 
                 </div>
             
-    // }
-    // else{ return <div>Загрузка...</div>}
+
 }
 
